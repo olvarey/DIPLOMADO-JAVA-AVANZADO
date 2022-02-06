79d0972dbc8c40da4bbb1322b65b6417b003e705
Melvin Reyes <olva.reyes@gmail.com>
2022-02-06 17:16:06 -0600

    Updated projects

--- 003-JVAM1S1/src/main/java/udb/net/mod1/regacad/spring/Principal.java
+++ 003-JVAM1S1/src/main/java/udb/net/mod1/regacad/spring/Principal.java
@@ -8,36 +8,13 @@ public class Principal {
 	private static ApplicationContext cont;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		// EstudianteCep e1 = new EstudianteCep(1, "Melvin", "Reyes", 8.45, 67.00);
-		// EstudianteColp e2 = new EstudianteColp(2, "Oswaldo", "Pineda", 9.90, 70.40);
 
 		cont = new FileSystemXmlApplicationContext("src/main/resources/applicationContext.xml");
-		Estudiante e1 = cont.getBean(EstudianteCep.class);
-		Estudiante e2 = cont.getBean(EstudianteColp.class);
-
 		RegistroAcad r1 = cont.getBean(RegistroAcad.class);
 
-		
-		
 		System.out.println("Registro ID: " + r1.getIdRegistro());
 		System.out.println("Registro fecha: " + r1.getFechaRegistro());
 		System.out.println("Estudiante: " + r1.getEstudiante().getNombre() + " " + r1.getEstudiante().getApellido());
-
-//		e1.setCodigo(1);
-//		e1.setNombre("Melvin");
-//		e1.setApellido("Reyes");
-//		e1.setCum(8.45);
-//		e1.setCuotaMensual(67.00);
-//
-//		e2.setCodigo(2);
-//		e2.setNombre("Oswaldo");
-//		e2.setApellido("Pineda");
-//		e2.setCum(9.90);
-//		e2.setCuotaMensual(70.40);
-
-//		imprimir(e1);
-//		imprimir(e2);
 
 	}
 
