13d5f2b9e1cc45b8381e39cad69261998453896c
Melvin Reyes <olva.reyes@gmail.com>
2022-02-26 19:33:37 -0600

    Added new project 0004POA

---
+++ 004POA/src/main/java/udb/net/mod1/poa/Principal.java
@@ -0,0 +1,10 @@
+package udb.net.mod1.poa;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.err.println("Hello world!");
+	}
+
+}
